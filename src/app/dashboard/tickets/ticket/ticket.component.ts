43917de099a8398214f79c13643affe733896fe0
Abderrahmane El-houij <abdoel132003@gmail.com>
2024-11-15 21:45:52 +0100

    refactor ticket component to use template variables and form submission handling

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './ticket.component.html',
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form')
 
+  onsubmit(title: string, ticketText: string) {
+    console.log(title);
+    console.log(ticketText);
+    this.form().nativeElement.reset();
+  }
 }
